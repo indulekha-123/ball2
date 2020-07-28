899e80e0b14a15c89a873aed76292c330a5d34d4
indulekha-123 <67064107+indulekha-123@users.noreply.github.com>
2020-07-28 18:57:13 +0530

    Add files via upload

---
+++ circle.js
@@ -0,0 +1,19 @@
+class Circle {
+    constructor(x,y,radius) {
+      var options={
+ "isStatic": false,
+"restitution":0.1,
+  " friction":0.5,     
+  " density":1.2  
+
+  
+ }
+ 
+
+  }
+    display(){
+     
+      circle(160,240,60);
+      
+    }
+  }
